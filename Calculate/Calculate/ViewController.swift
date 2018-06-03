98526f5a4c6075b1f705e34d9fbd74d8a62665eb
Takuma Osada <judgment9ttk069@gmail.com>
2018-06-03 15:16:28 +0900

    calculation

--- Calculate/Calculate/ViewController.swift
+++ Calculate/Calculate/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  SwiftButton1
+//  Calculate
 //
 //  Created by Takuma Osada on 2018/06/03.
 //  Copyright © 2018年 Takuma Osada. All rights reserved.
@@ -9,17 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  var count = 0
+  var plus = 0
+  var minus = 0
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    count = 10
+    count = count + 1
   }
-  
-  @IBAction func Tap(_ sender: Any) {
-    
-    
-  }
-  
+
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
--- SwiftButton1/SwiftButton1/NextViewController.swift
+++ SwiftButton1/SwiftButton1/NextViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class NextViewController: UIViewController {
 
+    @IBOutlet var changeLabel: UILabel!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+  
+    @IBAction func Change(_ sender: Any) {
+      
+      changeLabel.text = "鍵がかかりました"
+      
+    }
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
