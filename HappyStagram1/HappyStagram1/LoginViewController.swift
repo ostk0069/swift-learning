092edf58ee3fd17a0669f11ce1019ea7cb1bdd80
Takuma Osada <judgment9ttk069@gmail.com>
2018-07-02 16:57:18 +0900

    make firebase connected

---
+++ HappyStagram1/HappyStagram1/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  HappyStagram1
+//
+//  Created by Takuma Osada on 2018/07/02.
+//  Copyright © 2018年 Takuma Osada. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+  @IBOutlet var emailTextField: UITextField!
+  @IBOutlet var passwordTextField: UITextField!
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+  
+  @IBAction func createNewUser(_ sender: Any) {
+    
+  }
+  
+  @IBAction func userLogin(_ sender: Any) {
+    
+  }
+  
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+      
+    }
+  
+}
